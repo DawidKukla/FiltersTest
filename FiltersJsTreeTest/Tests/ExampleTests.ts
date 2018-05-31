8d9e247c6477685158187377a4d23d4d3926cbde
DawidKukla <kukladawid@gmail.com>
2018-05-31 16:59:52 +0200

    1

---
+++ FiltersJsTreeTest/Tests/ExampleTests.ts
@@ -0,0 +1,24 @@
+﻿import * as Mock from "typemoq";
+
+export interface ITest{
+    GetData():number;
+}
+
+describe("Test Suite", function () {
+    var a;
+
+    it("Example", function () {
+        a = true;
+        console.log("ABC")
+        expect(a).toBe(true);
+    });
+
+    it("Mock", function() {
+        var mock=Mock.Mock.ofType<ITest>();
+        mock.setup(x => x.GetData()).returns(() => 100);
+        expect(mock.target.GetData()).toBe(100);
+    });
+
+
+});
+
